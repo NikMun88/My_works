3c48b59b2d20c2674957024f28f1f23739352395
NikMun88 <for.study123@mail.ru>
2024-09-25 16:58:00 +0600

    some simple statistics with jupyter+pandas / Немного простой статистики на jupyter notebook + pandas
    Just some fun / Немного поигрался со статистикой

---
+++ Pandas/Pandas_work/main.py
@@ -0,0 +1,6 @@
+# Реализация пошагового прогроммирования с использованием jupyter notebook
+# Implementing step-by-step codding using jupyter notebook
+
+# command:
+# 'jupyter notebook' in console, open .ipynb in opened browser page
+# 'jupyter notebook' в консоль, отерыть .ipyng в открывшемся окне браузера
